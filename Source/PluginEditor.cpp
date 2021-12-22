8e0e11f7999a85fb51431a1d45f53074cfa3aa20
Jesús Valdez <jsvaldezv@yahoo.com>
2021-12-21 21:36:43 -0600

    Responde Curse #2

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -16,10 +16,27 @@ SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor (SimpleEQAudioProcess
         addAndMakeVisible(comp);
     }
     
+    const auto& params = audioProcessor.getParameters();
+    for(auto param : params)
+    {
+        param->addListener(this);
+    }
+    
+    startTimerHz(60);
+    
     setSize (600, 400);
 }
 
-SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor(){}
+SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor()
+{
+    const auto& params = audioProcessor.getParameters();
+    for(auto param : params)
+    {
+        param->removeListener(this);
+    }
+    
+    stopTimer();
+}
 
 void SimpleEQAudioProcessorEditor::paint (juce::Graphics& g)
 {
@@ -121,7 +138,11 @@ void SimpleEQAudioProcessorEditor::timerCallback()
 {
     if(parametersChanged.compareAndSetBool(false, true))
     {
+        auto chainSettings = getChainSettings(audioProcessor.apvts);
+        auto peakCoefficients = makePeakFilter(chainSettings, audioProcessor.getSampleRate());
+        updateCoefficients(monoChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
         
+        repaint();
     }
 }
 
