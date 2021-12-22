52ba38ecc60bb6e4eaba5a7bed8aa96a4cde3681
Jesús Valdez <jsvaldezv@yahoo.com>
2021-12-21 20:44:32 -0600

    Add attachments

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -25,13 +25,24 @@ private:
 
     SimpleEQAudioProcessor& audioProcessor;
     
-    CustomRotarySlider peakFreqSlider,
-    peakGainSlider,
-    peakQualitySlider,
-    lowCutFreqSlider,
-    highCutFreqSlider,
-    lowCutSlopeSlider,
-    highCutSlopeSlider;
+    CustomRotarySlider  peakFreqSlider,
+                        peakGainSlider,
+                        peakQualitySlider,
+                        lowCutFreqSlider,
+                        highCutFreqSlider,
+                        lowCutSlopeSlider,
+                        highCutSlopeSlider;
+    
+    using APVTS = juce::AudioProcessorValueTreeState;
+    using Attachment = APVTS::SliderAttachment;
+    
+    Attachment  peakFreqSliderAttachment,
+                peakGainSliderAttachment,
+                peakQualitySliderAttachment,
+                lowCutFreqSliderAttachment,
+                highCutFreqSliderAttachment,
+                lowCutSlopeSliderAttachment,
+                highCutSlopeSliderAttachment;
     
     std::vector<juce::Component*> getComps();
 
