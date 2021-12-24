cb0341f4d4eac50ae7bc67d5fa7d830b87e7781d
Jesús Valdez <jsvaldezv@yahoo.com>
2021-12-24 16:13:56 -0600

    Refactor #2

---
+++ Source/ResponseCurveComponent.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+#include "PathProducer.h"
+
+struct ResponseCurveComponent : juce::Component,
+                                juce::AudioProcessorParameter::Listener,
+                                juce::Timer
+{
+public:
+    
+    ResponseCurveComponent (SimpleEQAudioProcessor&);
+    ~ResponseCurveComponent() override;
+    
+    void paint (juce::Graphics& g) override;
+    
+    void parameterValueChanged (int parameterIndex, float newValue) override;
+    void parameterGestureChanged (int parameterIndex, bool gestureIsStarting) override;
+    
+    void timerCallback() override;
+    
+    void resized() override;
+    
+    void toggleAnalysisEnablement(bool enabled)
+    {
+        shouldShowFFTAnalysis = enabled;
+    }
+    
+private:
+    
+    juce::Atomic<bool> parametersChanged {false};
+    
+    MonoChain monoChain;
+    
+    void updateChain();
+    
+    SimpleEQAudioProcessor& audioProcessor;
+    
+    juce::Image background;
+    
+    juce::Rectangle<int> getRenderArea();
+    
+    juce::Rectangle<int> getAnalisisArea();
+    
+    PathProducer leftPathProducer, rightPathProducer;
+    
+    bool shouldShowFFTAnalysis = true;
+};
